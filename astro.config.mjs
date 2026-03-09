aa8824f69354d7c32f63f782f1325b2e1d90cd6f
D4VGFX <d4v.gfx@gmail.com>
2026-03-09 10:28:20 +0100

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://davstudios.it',
+  site: 'https://d4vgfx.github.io',
+  base: '/_davstudios',
 });
